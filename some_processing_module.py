119b013a992ecfbca4983b9bdc9fcffae5d8194c
RedwanPranta1 <112273347+RedwanPranta1@users.noreply.github.com>
2024-06-13 03:41:14 +0600

    some_processing_module.py

---
+++ some_processing_module.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def process(df, input_1, input_2):
+    # Example processing logic
+    df['new_column'] = df['existing_column'] * float(input_2) + len(input_1)  # Replace with actual logic
+    return df
